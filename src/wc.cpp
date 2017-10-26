28cea78b5ebacf73900ac0ccbaaad86a6c185d4a
Raul Silveira <raul95@gmail.com>
2017-10-26 15:17:04 -0300

    Sincronização sem prioridade

--- src/wc.cpp
+++ src/wc.cpp
@@ -1,5 +1,6 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <iostream>
+#include <unistd.h>
 #include <cstdlib>
 #include <fstream>
 #include <sstream>
@@ -8,71 +9,132 @@
 #include <chrono>
 #include <thread>
 #include <stdlib.h>   
-#include <time.h>      
+#include <time.h>  
 
 //#include "../includes/semaforo.h"
+#include "../includes/pessoa.h"
 #include "semaforo.cpp"
 
 using namespace std::chrono;
 using namespace std;
 
 int capacidade;
+int capacidadeAtual;
+int tamanhoFila;
 char sexo_usado;
 Semaforo *semaforo;
-Semaforo *print;
+Semaforo *semBin;
 
-void run(char s, int temp){
-	bool flag = true;
-	while(flag){
-		if(semaforo->getContador() == capacidade){
-			sexo_usado = s;
-			semaforo->P();
-			cout << "entrou " << s <<"\ncapacidade atual é " << semaforo->getContador() << "\nTempo de mijo" << temp <<"\n";
-			this_thread::sleep_for (seconds(temp));
-			semaforo->V();
-			cout << "saiu " << s <<"\n capacidade atual é " << semaforo->getContador() << "\n";
-			flag = false;
+void utilizar(Pessoa pessoa){
+	semaforo->P();
+	printf(" %c     %i          %i               %is        %s\n", pessoa.getSexo(), pessoa.getId(), (capacidade - semaforo->getContador()), pessoa.getTempo(), " Entrou");
+	this_thread::sleep_for(seconds(pessoa.getTempo()));
+	printf(" %c     %i          %i               %is        %s\n", pessoa.getSexo(), pessoa.getId(), (capacidade - semaforo->getContador()), pessoa.getTempo(), " Saiu");
+	semaforo->V();
+}
+
+void entrar(Pessoa pessoa){
+	while(true){
+		if(capacidade == capacidadeAtual){
+			semBin->P();
+			sexo_usado = pessoa.getSexo();
+			capacidadeAtual -= 1;
+			semBin->V();
+			utilizar(pessoa);
+			capacidadeAtual += 1;
+			return;
 		}else{
-			if(sexo_usado == s && semaforo->getContador() > 0){
-				semaforo->P();
-				cout << "entrou " << s <<"\ncapacidade atual é " << semaforo->getContador() << "\nTempo de mijo" << temp <<"\n";
-				this_thread::sleep_for (seconds(temp));
-				semaforo->V();
-				cout << "saiu " << s <<"\n capacidade atual é " << semaforo->getContador() << "\n";
-				flag = false;
+			semBin->V();
+			if(sexo_usado == pessoa.getSexo()){
+				capacidadeAtual -= 1;
+				utilizar(pessoa);
+				capacidadeAtual += 1;
+				return;
 			}
-
 		}
 	}
+
+	
+	// if(semaforo->getContador() == capacidade){
+	// 	semBin->P();
+	// 	sexo_usado = s;
+	// 	printf(" %c     %i          %i                 %is        %s\n", s, id, semaforo->getContador(), temp, " Entrou");
+	// 	utilizar(s, id, temp);
+	// 	semBin->V();
+	// }else{
+	// 	if(sexo_usado == s){
+	// 		printf(" %c     %i          %i                 %is        %s\n", s, id, semaforo->getContador(), temp, " Entrou");
+	// 		utilizar(s, id, temp);
+	// 	}
+	// }
+
+	
+	//printf(" %c     %i          %i                 %is        %s\n", s, id, semaforo->getContador(), temp, " Entrou");
+	// mutex->V();
+	// if(sexo_usado == s && semaforo->getContador() > 0){
+	// 	semaforo->P();
+	// 	printf(" %c     %i          %i                 %is        %s\n", s, id, semaforo->getContador(), temp, " Entrou");
+	// 	flag = false;
+	// }
 }
 
 int main(int argc, const char * argv[]){
 	
-	int sexo, temp;
-	cout << "Qual a capacidade do banheiro?\n";
-	cin >> capacidade;
+	int sexoInt, temp;
+	printf("Qual a capacidade do banheiro? ");
+	scanf("%i", &capacidade);
+	printf("Quantas pessoas na fila? ");
+	scanf("%i", &tamanhoFila);
+
+	capacidadeAtual = capacidade;
 
 	semaforo = new Semaforo(1230,capacidade,IPC_CREAT|0600);
+	semBin = new Semaforo(1231,1,IPC_CREAT|0600);
 
-	vector<thread> homens;
-	vector<thread> mulheres;
+	srand(time(NULL));
 
-	srand (time(NULL));
+	vector<Pessoa> pessoas;
+	vector<thread> fila;
 
-	while(true){
-		sexo = rand() % 2;
+	for(int i = 1; i <= tamanhoFila; i++){
+		sexoInt = rand() % 2;
 		temp = (rand() % 5) + 1;
-		//homem = 0; mulher = 1
-		if(sexo == 0){
-			cout << "chegou um homem \n";
-			homens.push_back(thread(run, 'H', temp));
+		char sexo;		
 
+		if(sexoInt == 0){
+			sexo = 'H';
 		}else{
-			cout << "chegou uma mulher \n";
-			mulheres.push_back(thread(run, 'M', temp));
+			sexo = 'M';
 		}
-		this_thread::sleep_for (seconds(1));
+
+		Pessoa p(sexo, i, temp);
+		pessoas.push_back(p);
 	}
+
+	printf("Sexo | Id | Qtd de pessoas | Tempo de uso |  Status\n");
+	printf("======================================================\n");
+
+	for(int i = 0; i < tamanhoFila; i++){
+		printf(" %c     %i          %i               %is        %s\n", pessoas[i].getSexo(), pessoas[i].getId(), (capacidade - semaforo->getContador()), pessoas[i].getTempo(), " Esperando");
+		fila.push_back(thread(entrar, pessoas[i]));
+
+		// if(sexo == 0){
+		// 	printf(" %c     %i          %i                 %is        %s\n", 'H', id, semaforo->getContador(), temp, " Esperando");
+		// 	fila.push_back(thread(entrar, 'H', temp, id));
+		// 	id++;
+		// }else{
+		// 	printf(" %c     %i          %i                 %is        %s\n", 'M', id, semaforo->getContador(), temp, " Esperando");
+		// 	fila.push_back(thread(entrar, 'M', temp, id));
+		// 	id++;
+		// }
+	}
+
+	for (int i = 0; i < tamanhoFila; i++){
+		fila[i].join();
+	}
+
+	
+	// 	this_thread::sleep_for (seconds(1));
 	
 	return 0;
 }
